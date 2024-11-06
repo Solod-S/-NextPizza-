511463daf9da44abd4108e2a33e3e122a77c85a2
Sergey <104456968+Solod-S@users.noreply.github.com>
2024-11-06 09:14:12 +0200

    project setup: added theme and shadcn/ui components..
    project setup: added theme and shadcn/ui components..

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+
+const nunito = Nunito({
+  subsets: ["cyrillic"],
+  variable: "--font-nunito",
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "🍕 Pizza Next - Freshly Baked Pizza Delivery 🍕",
+  description:
+    "Order delicious, freshly baked pizza with a variety of toppings! Fast delivery and great deals every day. Perfect for pizza lovers!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={nunito.variable}>
+        <main className="min-h-screen">{children}</main>
+      </body>
+    </html>
+  );
+}
